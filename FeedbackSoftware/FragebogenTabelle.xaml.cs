736e6ef367c13c72db5db6a8ef23ee867a8a8b15
Philipp Weber <philipp.weber@warptec.com>
2025-01-23 09:29:35 +0100

    kleine fixes und hinzufügen von radiobutton funktion

--- FeedbackSoftware/FragebogenTabelle.xaml.cs
+++ FeedbackSoftware/FragebogenTabelle.xaml.cs
@@ -23,5 +23,43 @@ namespace FeedbackSoftware
         {
             InitializeComponent();
         }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Message Sent!");
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            // Get the current checkbox
+            var currentCheckBox = sender as CheckBox;
+
+            // Get the Grid.Row of the current checkbox
+            int currentRow = Grid.GetRow(currentCheckBox);
+
+            // Get the parent Grid
+            var parent = VisualTreeHelper.GetParent(currentCheckBox);
+
+            while (parent != null && parent.GetType() != typeof(Grid))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            if (parent is Grid grid)
+            {
+                // Loop through all children in the Grid
+                foreach (UIElement child in grid.Children)
+                {
+                    if (child is CheckBox checkBox && checkBox != currentCheckBox)
+                    {
+                        // Uncheck checkboxes in the same Grid.Row
+                        if (Grid.GetRow(checkBox) == currentRow)
+                        {
+                            checkBox.IsChecked = false;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
