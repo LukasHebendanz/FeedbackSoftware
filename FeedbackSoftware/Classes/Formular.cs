094f16e412f15f6763821ac88022fd22a080a495
Ali Zeyn Gökyildiz <ali.goekyildiz@computop.com>
2025-01-23 09:43:34 +0100

    GUI: - Added FormularListWindow and TeacherWindow - Added Formular.cs Class - Added Binding to FormularListWindow

---
+++ FeedbackSoftware/Classes/Formular.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeedbackSoftware.Classes
+{
+    public class Formular : INotifyPropertyChanged
+    {
+        public Formular(){ }
+
+        #region Properties
+        private string formularName;
+        public string FormularName { get => formularName; set { formularName = value; OnPropertyChanged(); } }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FeedbackSoftware/FormularListWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using FeedbackSoftware.Classes;
+
+namespace FeedbackSoftware
+{
+    /// <summary>
+    /// Interaction logic for FormularListWindow.xaml
+    /// </summary>
+    public partial class FormularListWindow : Window
+    {
+        public List<Formular> Formularliste {  get; set; }
+
+        public FormularListWindow()
+        {
+            InitializeComponent();
+
+            Formularliste = new List<Formular>()
+            {
+                new Formular { FormularName = "Formular1_Schlüssel" },
+                new Formular { FormularName = "Formular2_Schlüssel" },
+                new Formular { FormularName = "Formular3_Schlüssel" }
+            };
+
+            DataContext = this;
+        }
+    }
+}
