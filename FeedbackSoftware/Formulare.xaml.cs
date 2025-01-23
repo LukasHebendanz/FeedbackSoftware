394500a605b818f57dee3e491b5199c7fd3b6beb
Lukas Hebendanz <lukahebe@by5019.schule-by.de>
2025-01-23 15:42:10 +0100

    Zielscheibenformular teilweise erstellt

--- FeedbackSoftware/Formulare.xaml.cs
+++ FeedbackSoftware/Formulare.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace FeedbackSoftware
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaktionslogik für Formulare.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Formulare : Window
     {
-        public MainWindow()
+        public Formulare()
         {
             InitializeComponent();
         }
-
     }
 }
