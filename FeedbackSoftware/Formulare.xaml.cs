50d244de424a163c870bab14041c9c9c79490f54
Lukas Hebendanz <lukahebe@by5019.schule-by.de>
2025-02-27 10:34:34 +0100

    Backend Logik für das Zielscheibenformular

--- FeedbackSoftware/Formulare.xaml.cs
+++ FeedbackSoftware/Formulare.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,71 @@ namespace FeedbackSoftware
     /// </summary>
     public partial class Formulare : Window
     {
+        private Dictionary<string, List<CheckBox>> checkBoxGroups = new Dictionary<string, List<CheckBox>>();
+
         public Formulare()
         {
             InitializeComponent();
+            InitializeCheckBoxGroups();
+        }
+
+        private void InitializeCheckBoxGroups()
+        {
+            var canvas = this.FindName("nuckelan") as Canvas; // Ersetzen Sie "YourCanvasName" durch den tatsächlichen Namen Ihres Canvas
+            if (canvas == null)
+            {
+                Debug.WriteLine("Canvas nicht gefunden!");
+                return;
+            }
+
+            var allCheckBoxes = canvas.Children.OfType<CheckBox>().ToList();
+            Debug.WriteLine($"Anzahl der gefundenen CheckBoxes: {allCheckBoxes.Count}");
+
+            foreach (var checkBox in allCheckBoxes)
+            {
+                string groupName = GetGroupName(checkBox.Content.ToString());
+                if (!checkBoxGroups.ContainsKey(groupName))
+                {
+                    checkBoxGroups[groupName] = new List<CheckBox>();
+                }
+                checkBoxGroups[groupName].Add(checkBox);
+                checkBox.Checked += CheckBox_Checked;
+            }
+
+            Debug.WriteLine($"Anzahl der Gruppen: {checkBoxGroups.Count}");
+            foreach (var group in checkBoxGroups)
+            {
+                Debug.WriteLine($"Gruppe {group.Key}: {group.Value.Count} CheckBoxes");
+            }
+        }
+
+        private string GetGroupName(string content)
+        {
+            // Extrahiert den Gruppennamen (z.B. "ObenLinks", "ObenRechts", etc.)
+            return content.Substring(0, content.LastIndexOf('_'));
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            var checkedBox = sender as CheckBox;
+            if (checkedBox != null)
+            {
+                string groupName = GetGroupName(checkedBox.Content.ToString());
+                if (checkBoxGroups.ContainsKey(groupName))
+                {
+                    foreach (var checkBox in checkBoxGroups[groupName])
+                    {
+                        if (checkBox != checkedBox)
+                        {
+                            checkBox.IsChecked = false;
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.WriteLine($"Gruppe {groupName} nicht gefunden!");
+                }
+            }
         }
     }
 }
