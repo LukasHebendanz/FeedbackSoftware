1fd2827d705d581e43910c9a41a30593b37116f4
Ali Zeyn Gökyildiz <ali.goekyildiz@computop.com>
2025-02-27 09:58:35 +0100

    Paging eingebaut mit neuer Teacherwindow welches nun Klasse erstellen und schlüssel erstellen und ansehen enthält

--- FeedbackSoftware/Views/Windows/TeacherWindow.xaml.cs
+++ FeedbackSoftware/Views/Windows/TeacherWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FeedbackSoftware.Views.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,18 @@ namespace FeedbackSoftware.Views
         public TeacherWindow()
         {
             InitializeComponent();
+
+            // Startseite festlegen
+            MainFrame.NavigationService.Navigate( new FeedbackKeyWindow());
+        }
+
+        private void NavigateToFeedbackPage_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate( new FeedbackKeyWindow());
+        }
+        private void NavigateToKlasse_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.NavigationService.Navigate(new SchoolClassPage());
         }
     }
 }
